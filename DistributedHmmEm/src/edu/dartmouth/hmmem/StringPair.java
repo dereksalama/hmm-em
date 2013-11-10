aad7f94e67ae78f0ac8dc3da93ba8e6e4debf053
Derek Salama <dereksalama@gmail.com>
2013-11-10 00:25:45 -0500

    stringpair

---
+++ DistributedHmmEm/src/edu/dartmouth/hmmem/StringPair.java
@@ -0,0 +1,18 @@
+package edu.dartmouth.hmmem;
+
+public class StringPair {
+	
+	public final String x;
+	public final String y;
+	
+	public StringPair(String x, String y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	//@Override
+	//public boolean Equals(Object o) {
+		
+	//}
+
+}
