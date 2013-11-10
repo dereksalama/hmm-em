612b7530b2c0dc98da6ecb8785c9e2515cdb9d48
Derek Salama <dereksalama@gmail.com>
2013-11-10 00:37:27 -0500

    pair done

--- DistributedHmmEm/src/edu/dartmouth/hmmem/StringPair.java
+++ DistributedHmmEm/src/edu/dartmouth/hmmem/StringPair.java
@@ -10,9 +10,20 @@ public class StringPair {
 		this.y = y;
 	}
 	
-	//@Override
-	//public boolean Equals(Object o) {
-		
-	//}
-
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof StringPair)) {
+			return false;
+		}
+		StringPair other = (StringPair) o;
+		return x.equals(other.x) && x.equals(other.x); 
+	}
+	
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = hash * 17 + x.hashCode();
+        hash = hash * 31 + y.hashCode();
+        return hash;
+    }
 }
