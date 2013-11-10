1b33f95be761ed52c71e69046b7e9918fcc32796
Derek Salama <dereksalama@gmail.com>
2013-11-09 21:55:23 -0500

    renamed package

--- DistributedHmmEm/src/edu/dartmouth/hmmem/MaximizationReducer.java
+++ DistributedHmmEm/src/edu/dartmouth/hmmem/MaximizationReducer.java
@@ -1,4 +1,4 @@
-package edu;
+package edu.dartmouth.hmmem;
 
 import java.io.IOException;
 import java.util.HashMap;
--- DistributedHmmEm/src/edu/dartmouth/hmmem/ValueAndCount.java
+++ DistributedHmmEm/src/edu/dartmouth/hmmem/ValueAndCount.java
@@ -1,4 +1,4 @@
-package edu;
+package edu.dartmouth.hmmem;
 
 import java.io.DataInput;
 import java.io.DataOutput;
